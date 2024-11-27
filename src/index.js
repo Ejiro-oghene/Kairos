9589ca06233d3ee053aa02c8c8c9e28bd115091f
Ejiro11 <kweenej@gmail.com>
2024-11-27 13:06:40 +0100

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@
 function regenerateWeather(response) {
   let temperatureElement = document.querySelector("#app-temperature");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
-
+  let descriptionElement = document.querySelector("#weather-description");
   let cityElement = document.querySelector("#app-city");
+  let humidityElement = document.querySelector("#app-humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = configureDate(date);
+  windElement.innerHTML = ` ${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = ` ${response.data.temperature.humidity}%`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+}
+
+function configureDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function citySearch(city) {
